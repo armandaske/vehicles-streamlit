a1549c93120ba3211b32cada3e8436ab19830d22
armandaske <armando.acevedoluna@gmail.com>
2025-02-23 19:33:12 -0600

    versión funcional con 3 gráficos

--- app.py
+++ app.py
@@ -10,12 +10,17 @@ import streamlit as st
 
 car_data = pd.read_csv('vehicles_us.csv')
 
-st.title('¡Bienvenido! \n Elige una opción para analizar las tendencias de venta de autos')
+st.title('¡Bienvenido!\nElige una opción para analizar las tendencias de venta de autos')
+st.write('\n')  # Dejar una separación entre título y casillas de verificación
 
-# crear una casilla de verificación
+# crear casillas de verificación
 build_histogram = st.checkbox('Construir un histograma para años de los autos')
 build_scatter = st.checkbox(
     'Construir un gráfico de dispersión entre odómetro y precio de venta')
+build_bar_plot = st.checkbox(
+    'Construir un gráfico de barras para el tipo de auto')
+
+st.write('\n')  # Dejar una separación entre casillas y gráficos
 
 if build_histogram:  # si la casilla de verificación está seleccionada
     st.write('Distribución de años de autos')
@@ -26,12 +31,16 @@ if build_histogram:  # si la casilla de verificación está seleccionada
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
 
-if build_scatter:  # si la casilla de verificación está seleccionada
-    st.write(
-        'Relación entre el odómetro y el precio de venta')
+if build_scatter:
+    st.write('Relación entre el odómetro y el precio de venta')
 
     # crear un gráfico de dispersión
     fig = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig, use_container_width=True)
 
-    # mostrar un gráfico Plotly interactivo
+if build_bar_plot:
+    st.write('Distribución de tipo de vehículo')
+
+    # crear un countplot
+    fig = px.histogram(car_data, x="type")
     st.plotly_chart(fig, use_container_width=True)
